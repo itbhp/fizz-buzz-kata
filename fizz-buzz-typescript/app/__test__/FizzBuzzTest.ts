5833489397ebee207f0094234ca8098fc53bae9e
Paolo Sciarra <itbhps@gmail.com>
2019-10-10 21:00:48 +0200

    now we covered number 2 transformation

--- fizz-buzz-typescript/app/__test__/FizzBuzzTest.ts
+++ fizz-buzz-typescript/app/__test__/FizzBuzzTest.ts
@@ -1,9 +1,16 @@
 function fizzBuzz(n: number): string {
+  if (n === 2) {
+    return '2';
+  }
   return '1';
 }
 
 describe('FizzBuzz Kata', () => {
   it('should give "1" for 1', () => {
     expect(fizzBuzz(1)).toEqual('1');
+  });
+
+  it('should give "2" for 2', () => {
+    expect(fizzBuzz(2)).toEqual('2');
   });
 });
