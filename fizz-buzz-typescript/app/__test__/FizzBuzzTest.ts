d3e80ab3fe6d4d7a2e976cd95aa8a0a58b5ab2f0
Paolo Sciarra <itbhps@gmail.com>
2019-10-10 20:56:39 +0200

    now we covered number 1 tranformation

---
+++ fizz-buzz-typescript/app/__test__/FizzBuzzTest.ts
@@ -0,0 +1,9 @@
+function fizzBuzz(n: number): string {
+  return '1';
+}
+
+describe('FizzBuzz Kata', () => {
+  it('should give "1" for 1', () => {
+    expect(fizzBuzz(1)).toEqual('1');
+  });
+});
---
+++ fizz-buzz-typescript/inbox.md
@@ -0,0 +1,9 @@
+## Test list
+
+- [X] test number 1
+- [ ] test number 2
+- [ ] test 3 -> Fizz
+- [ ] test 6 -> Fizz
+- [ ] test 5 -> Buzz
+- [ ] test 10 -> Buzz
+- [ ] test 15 -> FizzBuzz
