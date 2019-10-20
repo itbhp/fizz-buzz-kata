abd5e2cbc2cfab9f7f8686e145397706e9d1cc18
Paolo Sciarra <itbhps@gmail.com>
2019-10-20 19:41:30 +0200

    minor refactoring: extracted variable

--- fizz-buzz-typescript/app/source/FizzBuzz.ts
+++ fizz-buzz-typescript/app/source/FizzBuzz.ts
@@ -5,12 +5,11 @@ const combinatorFrom =
  (divisor: number) => (word: string) => (n: number) => n % divisor === 0 ? some(word) : none<string>();
 const fizz: (n: number) => Maybe<string> = combinatorFrom(3)('Fizz');
 const buzz: (n: number) => Maybe<string> = combinatorFrom(5)('Buzz');
-
+const combinators = [fizz, buzz];
 
 export function fizzBuzz(n: number): string {
-
   const monoid = maybeOrMonoid(stringMonoid);
-  return [fizz, buzz]
+  return combinators
     .map(combinator => combinator(n))
     .reduce(
       (acc: Maybe<string>, elem: Maybe<string>) =>
