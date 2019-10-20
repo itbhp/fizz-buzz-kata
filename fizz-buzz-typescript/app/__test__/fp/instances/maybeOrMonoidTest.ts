0884ab9598b84519cfeceae221b15a0df33355dd
Paolo Sciarra <itbhps@gmail.com>
2019-10-20 22:38:23 +0200

    fixed maybe monoid test

--- fizz-buzz-typescript/app/__test__/fp/instances/maybeOrMonoidTest.ts
+++ fizz-buzz-typescript/app/__test__/fp/instances/maybeOrMonoidTest.ts
@@ -1,22 +1,23 @@
-import { monoidLaws } from "../../../source/fp/types/Monoid";
 import { maybeOrMonoid, stringMonoid } from "../../../source/fp/instances/monoids";
+import { monoidLaws } from "../../../source/fp/types/Monoid";
 import { just } from "../../../source/fp/types/Maybe";
+
+
+const underTest = maybeOrMonoid(stringMonoid);
 
 describe('maybe or monoid', () => {
     it('should respect left identity', () => {
-        expect(monoidLaws.leftIdentity(maybeOrMonoid(stringMonoid))(just('test'))).toBe(true)
+        expect(monoidLaws.leftIdentity(underTest)(just('test'))).toBe(true)
     });
 
     it('should respect right identity', () => {
-        expect(monoidLaws.rightIdentity(maybeOrMonoid(stringMonoid))(just('test'))).toBe(true)
+        expect(monoidLaws.rightIdentity(underTest)(just('test'))).toBe(true)
     });
 
-    xit('should respect associativity', () => {
-        expect(monoidLaws.associativity(
-            maybeOrMonoid(stringMonoid))
-            (just('test'))
-            (just('associativity'))
-            (just('law'))
-        ).toBe(true)
+    it('should respect associativity', () => {
+        const a = just('test');
+        const b = just('associativity');
+        const c = just('law');
+        expect(monoidLaws.associativity(underTest)(a)(b)(c)).toEqual(true)
     });
 });
--- fizz-buzz-typescript/app/source/fp/types/Monoid.ts
+++ fizz-buzz-typescript/app/source/fp/types/Monoid.ts
@@ -1,12 +1,16 @@
+import deepEqual = require("deep-equal");
+
 export interface Monoid<T> {
     combine(a: T, b: T): T;
     identity: T;
 }
 
 export const monoidLaws = {
-  leftIdentity: <T>(monoid: Monoid<T>) => (t: T) => monoid.combine(monoid.identity, t) === t,
-  rightIdentity: <T>(monoid: Monoid<T>) => (t: T) => monoid.combine(t, monoid.identity) === t,
+  leftIdentity: <T>(monoid: Monoid<T>) => (t: T) => deepEqual(monoid.combine(monoid.identity, t), t),
+  rightIdentity: <T>(monoid: Monoid<T>) => (t: T) => deepEqual(monoid.combine(t, monoid.identity), t),
   associativity:  <T>(monoid: Monoid<T>) => (a: T) => (b: T) => (c: T) => 
-   monoid.combine(monoid.combine(a, b),c) ===
-   monoid.combine(a, monoid.combine(b,c))
+   deepEqual(
+     monoid.combine(monoid.combine(a, b),c),
+     monoid.combine(a, monoid.combine(b,c))
+   )
 }
