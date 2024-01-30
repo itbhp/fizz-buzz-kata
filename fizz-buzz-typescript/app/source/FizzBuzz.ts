3549cfc89c01c74178d321fec243698f286d5666
Paolo Sciarra <itbhps@gmail.com>
2024-01-30 18:00:14 +0100

    minor refactoring

--- fizz-buzz-typescript/app/source/FizzBuzz.ts
+++ fizz-buzz-typescript/app/source/FizzBuzz.ts
@@ -1,10 +1,12 @@
 import { Maybe, just, none } from "./fp/types/Maybe";
 import { maybeOrMonoid, stringMonoid } from "./fp/instances/monoids";
 
+type Rule = (n: number) => Maybe<string>
+
 const from = 
  (divisor: number) => (word: string) => (n: number) => n % divisor === 0 ? just(word) : none<string>();
-const fizz: (n: number) => Maybe<string> = from(3)('Fizz');
-const buzz: (n: number) => Maybe<string> = from(5)('Buzz');
+const fizz: Rule = from(3)('Fizz');
+const buzz: Rule = from(5)('Buzz');
 
 export function fizzBuzz(n: number): string {
   const byFirst = (acc: Maybe<string>, elem: Maybe<string>) => maybeOrMonoid(stringMonoid).combine(acc, elem);
